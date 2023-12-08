bac9d8b1ddf29dfe8942fc5e285ca1d1b3b726e9
Daogang Tang <daogangtang@gmail.com>
2023-12-08 15:21:38 +0800

    remove unused lines.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -19,33 +19,10 @@ fn main() -> Result<(), slint::PlatformError> {
         if let Err(_) = llmengin::start_engine(ui_handle, receiver) {
             // process before exit.
         }
-        
-        // loop {
-        //     let ui_handle = ui_handle.clone();
-        //     let _ask = receiver.recv().unwrap();
-        //     if _ask == "_exit_" {
-        //         break;
-        //     }
-        //     let reply = _ask;
-        //     update_dialog(ui_handle, reply);
-        //     // _ = slint::invoke_from_event_loop(move || {
-        //     //     let ui_handle = ui_handle.unwrap();
-        //     //     let old_content = ui_handle.get_dialog();
-        //     //     ui_handle.set_dialog(old_content + &reply);
-        //     // });
-        // }
     });
-
-    // let ui_handle = ui.as_weak();
-    // ui.on_request_increase_value(move || {
-    //     let ui = ui_handle.unwrap();
-    //     ui.set_counter(ui.get_counter() + 1);
-    // });
 
     let ui_handle = ui.as_weak();
     ui.on_send_ask_content(move |content| {
-        // let ui = ui_handle.unwrap();
-        // ui.set_dialog(ui.get_dialog() + &content);
         update_dialog(ui_handle.clone(), content.to_string());
         sender.send(content.to_string()).unwrap();
     });
